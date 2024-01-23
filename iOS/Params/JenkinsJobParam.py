75d1ed119458c7c36b4dac9ec98ad55d46702d78
feng.guo1 <feng.guo1@centurygame.com>
2024-01-23 20:17:20 +0800

    test5

--- iOS/Params/JenkinsJobParam.py
+++ iOS/Params/JenkinsJobParam.py
@@ -13,3 +13,21 @@ class JenkinsJobParam:
     _adjust_universal_link = None
     _ad_option = None
     _ddCommon = DDCommonParam
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('CGError : param missing', flush=True)
+        sys.exit(1)
+        
+    game_id = sys.argv[1]
+    push_enable = sys.argv[2]
+    firebase_enable = sys.argv[3]
+    job_params = sys.argv[4]
+    
+    JenkinsJobParam._game_id = game_id
+    JenkinsJobParam._push_enable = push_enable
+    JenkinsJobParam._firebase_enable = firebase_enable
+    print("job_params : ")
+    print(job_params)
+    
